42d8152719e9a72b9dadb5b382c7cbdd3ace278a
William Cummings <wcummings@forensiq.com>
2016-02-01 12:30:30 -0500

    Ignore CAP command for ircv3 "compat"

--- src/eircd_irc_protocol_fsm.erl
+++ src/eircd_irc_protocol_fsm.erl
@@ -47,6 +47,9 @@ pass({irc, {_, <<"PASS">>, [Pass], _}}, State) ->
     {next_state, nick_and_user, State#state{pass_provided = binary_to_list(Pass)}};
 pass(M, State) -> nick_and_user(M, State).
 
+;; Ignore CAP so we don't break v3 clients
+nick_and_user({irc, {_, <<"CAP">>, _, _}}, State) ->
+    {next_state, nick_and_user, State};
 nick_and_user({irc, {_, <<"NICK">>, [Nick], _}}, State=#state{nick=undefined}) ->
     case eircd_server:nick(Nick) of
         {error, nicknameinuse} ->
