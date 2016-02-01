a39ba359db6b0a3d1c08b66ec212b155e7390254
William Cummings <wcummings@forensiq.com>
2016-02-01 13:39:45 -0500

    Match but ignore passwords for now

--- src/eircd_irc_protocol_fsm.erl
+++ src/eircd_irc_protocol_fsm.erl
@@ -105,7 +105,7 @@ connected({irc, {_, <<"NICK">>, [NewNick], _}}, State=#state{nick=Nick, user=Use
             eircd_channel:nick(self(), Nick, NewNick)
     end,
     {next_state, connected, State};
-connected({irc, {_, <<"PRIVMSG">>, [Targets], MessageText}}, State) when is_list(Targets) ->
+connected({irc, {_, <<"PRIVMSG">>, [Targets|_], MessageText}}, State) when is_list(Targets) ->
     eircd_ping_fsm:mark_activity(State#state.ping_fsm),
     State2 = lists:foldl(
                fun(Target, S) ->
@@ -114,7 +114,7 @@ connected({irc, {_, <<"PRIVMSG">>, [Targets], MessageText}}, State) when is_list
                State,
                Targets),
     {next_state, connected, State2};
-connected({irc, {_, <<"PRIVMSG">>, [Target], MessageText}}, State) ->
+connected({irc, {_, <<"PRIVMSG">>, [Target|_], MessageText}}, State) ->
     eircd_ping_fsm:mark_activity(State#state.ping_fsm),
     {next_state, connected, privmsg(Target, MessageText, State)};
 connected({irc, {_, <<"JOIN">>, [Channels], _}}, State) when is_list(Channels) ->
